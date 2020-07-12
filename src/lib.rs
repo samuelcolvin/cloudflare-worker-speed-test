6f3f80dd1b7d46a9b4c3fd37294e8052cb104601
Samuel Colvin <s@muelcolvin.com>
2020-07-12 13:13:36 +0100

    regex feature working

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,30 @@ use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
     if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
+        use wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
 }
 
-#[wasm_bindgen]
-pub fn render_template(template: String, name: String) -> String {
-    (template + "\n\n(rust, simple replace)").replace("{{ name }}", &name)
+cfg_if! {
+    if #[cfg(feature = "simple_replace")] {
+        #[wasm_bindgen]
+        pub fn render_template(template: String, name: String) -> String {
+            (template + "\n\n(rust, simple replace)").replace("{{ name }}", &name)
+        }
+    }
+}
+
+
+cfg_if! {
+    if #[cfg(feature = "regex")] {
+        use regex::Regex;
+        #[wasm_bindgen]
+        pub fn render_template(template: String, name: String) -> String {
+            let name_re = Regex::new(r"\{\{ ?name ?\}\}").unwrap();
+            return name_re.replace_all(&(template + "\n\n(rust, regex)"), name.as_str()).to_string();
+        }
+    }
 }
